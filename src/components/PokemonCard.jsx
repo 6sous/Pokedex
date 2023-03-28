5fc89a4bf1a1ce257046077d4242b1017b489e5d
6sous <cyrille.aquilina@gmail.com>
2023-03-28 12:05:47 +0200

    made the pokemon img dynamique

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,22 @@
 function PokemonCard() {
-    return  <figure>
-                <figcaption>Bulbasaur</figcaption>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+    const pokemon = pokemonList[1]
+
+    return  <figure className="card">
+                <figcaption className="img-title">{pokemon.name}</figcaption>
+                {pokemon.imgSrc? <img style ={{width: 400,}} src={pokemon.imgSrc} alt="" /> : <p>???</p>}
+                
             </figure>
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard

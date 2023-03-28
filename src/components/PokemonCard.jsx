f1223cd0b5790477e870abaebf8ddd9b3b7c1e91
6sous <cyrille.aquilina@gmail.com>
2023-03-28 17:12:17 +0200

    quest react basics 5 completed

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,15 @@
+import PropTypes from "prop-types"
+
 function PokemonCard({pokemon}) {
-    const {name, imgSrc} = pokemon
-    
+    const {name, imgSrc} = pokemon;
+
+    PokemonCard.propTypes = {
+        pokemon: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            imgSrc: PropTypes.string,
+        }).isRequired
+    }
+
     return  <figure className="card">
                 <figcaption className={`img-title`}>{name}</figcaption>
                 {imgSrc? <img style ={{width: 400,}} src={imgSrc} alt="" /> : <p>???</p>}

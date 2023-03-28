959f0af19c84c9681a5ebbd158d11242674c1a9c
6sous <cyrille.aquilina@gmail.com>
2023-03-28 16:34:34 +0200

    quest react basics 4 completes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,22 +1,11 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1]
-
+function PokemonCard({pokemon}) {
+    const {name, imgSrc} = pokemon
+    
     return  <figure className="card">
-                <figcaption className="img-title">{pokemon.name}</figcaption>
-                {pokemon.imgSrc? <img style ={{width: 400,}} src={pokemon.imgSrc} alt="" /> : <p>???</p>}
+                <figcaption className={`img-title`}>{name}</figcaption>
+                {imgSrc? <img style ={{width: 400,}} src={imgSrc} alt="" /> : <p>???</p>}
                 
             </figure>
 }
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
 export default PokemonCard
